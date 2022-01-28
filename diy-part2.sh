f65340215209a24adcbfe055050033f8d6eb7384
Kinoko <ye8980@gmail.com>
2022-01-28 17:12:08 +0800

    修改默认主题配置

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,4 +20,4 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='Soft_Router'' p
 sed -i "s/OpenWrt /Kinoko build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # 修改默认主题
-sed -i "s/luci-theme-bootstrap/luci-theme-argon_new/g" feeds/luci/collections/luci/Makefile
+sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" feeds/luci/collections/luci/Makefile
